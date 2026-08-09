e1ac901a7af45ebaa2a53ca2213a68c662c7780d
Gris Ge <cnfourt@gmail.com>
2026-08-09 21:27:36 +0800

    Enable out.yaml tests (inline document markers)

--- src/tests/yaml_test_suite.rs
+++ src/tests/yaml_test_suite.rs
@@ -47,6 +47,8 @@ const SUPPORTED_OUT_YAML_TEST: &[&str] = &[
     "block-mapping-with-multiline-scalars",
     "block-mappings-in-block-sequence",
     "block-scalar-indicator-order",
+    "block-scalar-strip",
+    "block-scalar-strip-1-3",
     "block-sequence-in-block-mapping",
     "block-sequence-indentation",
     "colon-and-adjacent-value-after-comment-on-next-line",
@@ -66,6 +68,7 @@ const SUPPORTED_OUT_YAML_TEST: &[&str] = &[
     "empty-lines-between-mapping-elements",
     "escaped-slash-in-double-quotes",
     "explicit-key-and-value-seperated-by-comment",
+    "explicit-non-specific-tag-1-3",
     "flow-mapping",
     "flow-mapping-edge-cases",
     "flow-mapping-in-block-sequence",
@@ -74,6 +77,7 @@ const SUPPORTED_OUT_YAML_TEST: &[&str] = &[
     "flow-sequence-in-flow-mapping",
     "flow-sequence-in-flow-sequence",
     "folded-block-scalar",
+    "folded-block-scalar-1-3",
     "implicit-flow-mapping-key-on-one-line",
     "inline-tabs-in-double-quoted/01",
     "inline-tabs-in-double-quoted/02",
@@ -83,6 +87,7 @@ const SUPPORTED_OUT_YAML_TEST: &[&str] = &[
     "legal-tab-after-indentation",
     "literal-block-scalar",
     "literal-unicode",
+    "mapping-key-and-flow-sequence-item-anchors",
     "mixed-block-mapping-explicit-to-implicit",
     "mixed-block-mapping-implicit-to-explicit",
     "multiline-double-quoted-flow-mapping-key",
@@ -93,119 +98,209 @@ const SUPPORTED_OUT_YAML_TEST: &[&str] = &[
     "nested-flow-collections",
     "nested-flow-collections-on-one-line",
     "nested-flow-mapping-sequence-and-mappings",
-    "nested-implicit-complex-keys",
-    "nested-top-level-flow-mapping",
-    "node-anchor-and-tag-on-seperate-lines",
-    "node-and-mapping-key-anchors",
-    "node-and-mapping-key-anchors-1-3",
-    "plain-mapping-key-ending-with-colon",
-    "plain-url-in-flow-mapping",
-    "question-mark-edge-cases/00",
-    "question-mark-edge-cases/01",
-    "question-marks-in-scalars",
-    "scalars-in-flow-start-with-syntax-char/00",
-    "scalars-in-flow-start-with-syntax-char/01",
-    "sequence-entry-that-looks-like-two-with-wrong-indentation",
-    "sequence-indent",
-    "single-block-sequence-with-anchor",
-    "single-character-streams/00",
-    "single-character-streams/01",
-    "single-pair-implicit-entries",
-    "spec-example-2-10-node-for-sammy-sosa-appears-twice-in-this-document",
-    "spec-example-2-11-mapping-between-sequences",
-    "spec-example-2-12-compact-nested-mapping",
-    "spec-example-2-15-folded-newlines-are-preserved-for-more-indented-and-blank-lines",
-    "spec-example-2-16-indentation-determines-scope",
-    "spec-example-2-17-quoted-scalars",
-    "spec-example-2-18-multi-line-flow-scalars",
-    "spec-example-2-2-mapping-scalars-to-scalars",
-    "spec-example-2-3-mapping-scalars-to-sequences",
-    "spec-example-2-4-sequence-of-mappings",
-    "spec-example-2-5-sequence-of-sequences",
-    "spec-example-2-6-mapping-of-mappings",
-    "spec-example-2-9-single-document-with-two-comments",
-    "spec-example-5-12-tabs-and-spaces",
-    "spec-example-5-3-block-structure-indicators",
-    "spec-example-5-4-flow-collection-indicators",
-    "spec-example-5-5-comment-indicator",
-    "spec-example-5-6-node-property-indicators",
-    "spec-example-5-7-block-scalar-indicators",
-    "spec-example-6-1-indentation-spaces",
-    "spec-example-6-10-comment-lines",
-    "spec-example-6-11-multi-line-comments",
-    "spec-example-6-12-separation-spaces",
-    "spec-example-6-13-reserved-directives-1-3",
-    "spec-example-6-2-indentation-indicators",
-    "spec-example-6-22-global-tag-prefix",
-    "spec-example-6-23-node-properties",
-    "spec-example-6-24-verbatim-tags",
-    "spec-example-6-28-non-specific-tags",
-    "spec-example-6-3-separation-spaces",
-    "spec-example-6-4-line-prefixes",
-    "spec-example-6-5-empty-lines",
-    "spec-example-6-5-empty-lines-1-3",
-    "spec-example-6-6-line-folding",
-    "spec-example-6-7-block-folding",
-    "spec-example-6-8-flow-folding",
-    "spec-example-6-9-separated-comment",
-    "spec-example-7-10-plain-characters",
-    "spec-example-7-11-plain-implicit-keys",
-    "spec-example-7-13-flow-sequence",
-    "spec-example-7-14-flow-sequence-entries",
-    "spec-example-7-15-flow-mappings",
-    "spec-example-7-16-flow-mapping-entries",
-    "spec-example-7-18-flow-mapping-adjacent-values",
-    "spec-example-7-19-single-pair-flow-mappings",
-    "spec-example-7-2-empty-content",
-    "spec-example-7-20-single-pair-explicit-entry",
-    "spec-example-7-23-flow-content",
-    "spec-example-7-4-double-quoted-implicit-keys",
-    "spec-example-7-5-double-quoted-line-breaks",
-    "spec-example-7-6-double-quoted-lines",
-    "spec-example-7-8-single-quoted-implicit-keys",
-    "spec-example-8-1-block-scalar-header",
-    "spec-example-8-14-block-sequence",
-    "spec-example-8-15-block-sequence-entry-types",
-    "spec-example-8-16-block-mappings",
-    "spec-example-8-17-explicit-block-mapping-entries",
-    "spec-example-8-2-block-indentation-indicator",
-    "spec-example-8-20-block-node-types",
-    "spec-example-8-21-block-scalar-nodes",
-    "spec-example-8-21-block-scalar-nodes-1-3",
-    "spec-example-8-22-block-collection-nodes",
-    "spec-example-8-4-chomping-final-line-break",
-    "spec-example-8-5-chomping-trailing-lines",
-    "spec-example-8-6-empty-scalar-chomping",
-    "spec-example-8-7-literal-scalar",
-    "spec-example-8-8-literal-content",
-    "spec-example-8-9-folded-scalar",
-    "tab-at-beginning-of-line-followed-by-a-flow-mapping",
-    "tabs-in-various-contexts/001",
-    "tabs-in-various-contexts/002",
-    "tabs-in-various-contexts/010",
-    "tags-for-block-objects",
-    "tags-for-flow-objects",
-    "tags-in-block-sequence",
-    "tags-in-explicit-mapping",
-    "tags-in-implicit-mapping",
-    "tags-on-empty-scalars",
-    "three-dashes-and-content-without-space",
-    "three-explicit-integers-in-a-block-sequence",
-    "trailing-spaces-after-flow-collection",
-    "trailing-tabs-in-double-quoted/00",
-    "trailing-tabs-in-double-quoted/01",
-    "trailing-tabs-in-double-quoted/02",
-    "trailing-tabs-in-double-quoted/03",
-    "trailing-tabs-in-double-quoted/04",
-    "trailing-tabs-in-double-quoted/05",
-    "various-combinations-of-explicit-block-mappings",
-    "various-location-of-anchors-in-flow-sequence",
-    "various-trailing-comments",
-    "various-trailing-comments-1-3",
-    "various-trailing-tabs",
-    "whitespace-after-scalars-in-flow",
-    "whitespace-around-colon-in-mappings",
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
